4661ba10a159e82a0f4ead0c3ffe4d319e9053a3
Davi Seagull <daviseagull@ibm.com>
2023-08-10 13:10:13 -0300

    chore: add bearer token option in OpenApi

--- src/main/java/com/dseagull/taskmanagement/shared/OpenApi.java
+++ src/main/java/com/dseagull/taskmanagement/shared/OpenApi.java
@@ -1,12 +1,17 @@
 package com.dseagull.taskmanagement.shared;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApi {
+
+    public static final String SECURITY_TYPE = "bearer-jwt";
 
     private static Info getInfo() {
         return new Info()
@@ -18,6 +23,24 @@ public class OpenApi {
     @Bean
     public OpenAPI customizeOpenAPI() {
         return new OpenAPI()
-                .info(getInfo());
+                .info(getInfo())
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_TYPE))
+                .components(getComponents());
+
+    }
+
+    private static Components getComponents() {
+        return new Components()
+                .addSecuritySchemes(
+                        SECURITY_TYPE,
+                        getSecurityScheme());
+    }
+
+    private static SecurityScheme getSecurityScheme() {
+        return new SecurityScheme()
+                .name(SECURITY_TYPE)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
     }
 }
